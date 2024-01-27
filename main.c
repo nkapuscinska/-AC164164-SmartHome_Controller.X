27cb918720df4e2263cc923773d299ee56571e74
SabinaSlusarczyk <115367173+SabinaSlusarczyk@users.noreply.github.com>
2024-01-27 20:54:51 +0100

    Add files via upload
    Main z testem funkcji ADC_ReadLightDensity i funkcji LightCheck.

--- main.c
+++ main.c
@@ -8,7 +8,9 @@
 #include "mcc_generated_files/delay.h"
 #include "mcc_generated_files/drivers/timeout.h"
 #include "mcc_generated_files/pin_manager.h"
+#include "mcc_generated_files/adc1.h"
 #include <stdio.h>
+#include "xc.h" 
 
 /*
                          Main application
@@ -17,18 +19,26 @@ int main(void)
 {
     // initialize the device
     SYSTEM_Initialize();
+    ADC1_Initialize();
+    ADC1_Enable();
+    ADC1_ChannelSelect(channel_AN8);
+    ADC1_SoftwareTriggerEnable();
 
-    app_commModuleInit();               //inicjalizacja modulu komunikacyjnego (mqtt))
-    app_updateTemperature(0.5);         //ustawienie wartosci temoratury do wyslania
-
+//    app_commModuleInit();               //inicjalizacja modulu komunikacyjnego (mqtt))
+//    app_updateTemperature(0.5);         //ustawienie wartosci temoratury do wyslania
+    float light;
+    char lighton;
 
     while (1)
     {
         // Add your application code
         DELAY_milliseconds(40);         //male opoznienie dla uC (nie musimy sie spieszyc))
         app_mqttScheduler();            //scheduler mqtt - obsluguje cala komunikacje, wysyla dane raz na sekunde
+      
+        ADC_ReadLightDensity(); // try read light density.
         
         
+        lighton = LightCheck(light);
         /* Przyklad uzycia z przyciskami */
         /* wcisniecie przycisku powoduje zapalenie odpowiedniej 
          * diody LED i aktualizuje stan do wyslania przez mqtt*/
