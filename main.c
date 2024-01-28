1afac035c9afe8078901643fad6302853b4825cb
SabinaSlusarczyk <115367173+SabinaSlusarczyk@users.noreply.github.com>
2024-01-29 00:10:49 +0100

    main z testem temperatury

--- main.c
+++ main.c
@@ -8,27 +8,47 @@
 #include "mcc_generated_files/delay.h"
 #include "mcc_generated_files/drivers/timeout.h"
 #include "mcc_generated_files/pin_manager.h"
+#include "mcc_generated_files/adc1.h"
 #include <stdio.h>
+#include "xc.h" 
+#include "mcc_generated_files/MCP9808Drivers/MCP9808.h"
+#include "mcc_generated_files/i2c2.h"
 
 /*
                          Main application
  */
+extern uint16_t temperatureReady;
+
 int main(void)
 {
     // initialize the device
     SYSTEM_Initialize();
+    ADC1_Initialize();
+    ADC1_Enable();
+    ADC1_ChannelSelect(channel_AN8);
+    ADC1_SoftwareTriggerEnable();
+    
+    MCP9808_i2c_params i2c_params;
+    MCP9600_alert_flags alert_flags;
+    i2c_params.i2cAddress = 0x18;
+    i2c_params.i2cBusID = 2;
+    
+    MCP9808_MODULE_Initialize(i2c_params);
+    MCP9808_SetResolutionValue(i2c_params, 0.125);
+    
+    TMR1_Start();
 
-    app_commModuleInit();               //inicjalizacja modulu komunikacyjnego (mqtt))
-    app_updateTemperature(0.5);         //ustawienie wartosci temoratury do wyslania
-
-
+    
+//    app_commModuleInit();               //inicjalizacja modulu komunikacyjnego (mqtt))
+//    app_updateTemperature(0.5);         //ustawienie wartosci temoratury do wyslania
+  
     while (1)
     {
         // Add your application code
         DELAY_milliseconds(40);         //male opoznienie dla uC (nie musimy sie spieszyc))
         app_mqttScheduler();            //scheduler mqtt - obsluguje cala komunikacje, wysyla dane raz na sekunde
-        
-        
+        readAndDisplayTemperature();
+     
         /* Przyklad uzycia z przyciskami */
         /* wcisniecie przycisku powoduje zapalenie odpowiedniej 
          * diody LED i aktualizuje stan do wyslania przez mqtt*/
